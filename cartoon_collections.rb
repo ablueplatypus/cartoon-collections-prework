d7d0067d3d3443c38193df6ec70418e79096d6fb
Matt Doering <mattdoering86@gmail.com>
2018-11-07 21:31:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,8 @@ def long_planeteer_calls(array_of_calls)
   array_of_calls.any? { |words| words.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  string
 end
