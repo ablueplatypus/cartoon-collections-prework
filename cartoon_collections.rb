fa91c7c1cf686a04462521d8ae9de1dfa81a22cf
Matt Doering <mattdoering86@gmail.com>
2018-11-07 21:33:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 def find_the_cheese(array_of_strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  value = array_of_strings.include(cheese_types)
 end
