915b98c213d6d524c2af96bde8c26392dc4f566e
Matt Doering <mattdoering86@gmail.com>
2018-11-07 20:51:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  message = ""
+  array.each_with_index do |names, number|
+    puts number +". " + names
+  end
 end
 
 def summon_captain_planet# code an argument here
